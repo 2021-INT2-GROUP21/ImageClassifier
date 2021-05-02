59a5e9a54366a506029c00f888a427fcaed5e17c
Mahir Rahman <mahirrahman2000@gmail.com>
2021-05-02 15:17:28 +0100

    Added plotting of loss and acc

--- visualiser.py
+++ visualiser.py
@@ -1,4 +1,4 @@
-from models.SplitClassifier import SplitClassifier
+from models.TripleConvBlocks import CIFAR10Model
 from util import *
 from torch.utils.tensorboard import SummaryWriter
 import torchinfo
@@ -19,14 +19,54 @@ def main(in_model):
     dataiter = iter(train_loader)
     images, labels = dataiter.next()
 
-    model = SplitClassifier()
+    model = in_model
 
     writer.add_graph(model, images)
     writer.close()
 
     batch_size=32
     torchinfo.summary(model, input_size=(batch_size, 3, 32, 32))
+    plt_graph()
+
+
+def plt_graph():
+    import matplotlib.pyplot as plt
+    import csv
+    import numpy as np
+
+    with open('datapoints.csv', newline="") as f:
+        reader = csv.reader(f)
+        data = list(reader)
+
+    taccuracy = list(map(float, data[0][1:]))
+    tloss = list(map(float, data[1][1:]))
+    vaccuracy = list(map(float, data[2][1:]))
+    vloss = list(map(float, data[3][1:]))
+    x = list(range(1, len(taccuracy) + 1))
+
+    lossplot = plt.figure(1, figsize=(8, 8))
+    plt.title("Loss Over Epoch")
+    plt.plot(x, tloss, label="training loss")
+    plt.plot(x, vloss, label="validation loss")
+    plt.yticks(np.arange(0, 1.2, step=0.1))
+    plt.grid(True)
+    plt.xlabel("epoch")
+    plt.ylabel("loss")
+    plt.legend()
+
+    accloss = plt.figure(2, figsize=(8, 8))
+    plt.title("Accuracy Over Epoch")
+    plt.plot(x, taccuracy, label="training accuracy")
+    plt.plot(x, vaccuracy, label="validation accuracy")
+    plt.yticks(np.arange(0.55, 1, step=0.05))
+    plt.grid(True)
+    plt.xlabel("epoch")
+    plt.ylabel("accuracy")
+    plt.legend()
+
+    plt.show()
 
 
 if __name__ == '__main__':
-    main(SplitClassifier())
+    #plt_graph()
+    main(CIFAR10Model())
